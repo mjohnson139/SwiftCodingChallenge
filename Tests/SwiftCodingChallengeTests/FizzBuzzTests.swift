df4a7ddf54eb9ddf173cf5676fdaf01bfb88fb46
Matt Johnson <matt@roofsnap.com>
2023-08-07 16:57:52 -0400

    Updated the Fizzbuzz test.

--- Tests/SwiftCodingChallengeTests/FizzBuzzTests.swift
+++ Tests/SwiftCodingChallengeTests/FizzBuzzTests.swift
@@ -24,33 +24,29 @@ class FizzBuzzTests: XCTestCase {
         XCTAssertEqual(result[30], fizzBuzz)
     }
     
-    func testGeneralNumbers() {
-        let fizzNumbers = [3, 6, 9, 12, 18, 21, 24, 27, 33, 36, 39, 42, 48, 51, 54, 57, 63, 66, 69, 72, 78, 81, 84, 87, 93, 99]
-        let buzzNumbers = [5, 10, 20, 25, 35, 40, 50, 55, 65, 70, 80, 85, 95]
+    func testAllFizzBuzzNumbers() {
+        let fizzNumbers = [3, 6, 9, 12, 18, 21, 24, 27, 33, 36, 39, 42, 48, 51, 54, 57, 63, 66, 69, 72, 78, 81, 84, 87, 93, 96, 99]
+        let buzzNumbers = [5, 10, 20, 25, 35, 40, 50, 55, 65, 70, 80, 85, 95, 100]
         let fizzBuzzNumbers = [15, 30, 45, 60, 75, 90]
-        
-        var remainingNumbers = [Int]()
-        for i in 1...100 {
-            if !fizzNumbers.contains(i) && !buzzNumbers.contains(i) && !fizzBuzzNumbers.contains(i) {
-                remainingNumbers.append(i)
-            }
-        }
-        
+
         let actual = codingChallenge.fizzBuzz()
+
         for n in fizzNumbers {
-            XCTAssertEqual(actual[n], "Fizz")
+            XCTAssertEqual(actual[n], "Fizz", "Error for number \(n)")
         }
-        
+
         for n in buzzNumbers {
-            XCTAssertEqual(actual[n], "Buzz")
+            XCTAssertEqual(actual[n], "Buzz", "Error for number \(n)")
         }
-        
+
         for n in fizzBuzzNumbers {
-            XCTAssertEqual(actual[n], "FizzBuzz")
+            XCTAssertEqual(actual[n], "FizzBuzz", "Error for number \(n)")
         }
-        
+
+        // Check that the remaining numbers are empty strings in the result
+        let remainingNumbers = Set(1...100).subtracting(fizzNumbers + buzzNumbers + fizzBuzzNumbers)
         for n in remainingNumbers {
-            XCTAssertEqual(actual[n], "")
+            XCTAssertEqual(actual[n], "", "Error for number \(n)")
         }
     }
 }
